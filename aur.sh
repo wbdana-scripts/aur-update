180e54e7c42827b643a55aae89e0892e8d7b86bc
William Dana <william.b.dana@gmail.com>
2018-08-18 12:16:06 -0400

    Fix indentation

--- aur.sh
+++ aur.sh
@@ -34,97 +34,97 @@ printf "Starting updates...\n${NC}"
 
 for d in ./*/; do
 	cd "$d"
-		# Get current directory string length
-		DIR_NAME_LEN=${#d}
-		# Remove './' from start of current directory
-		PKG_NAME=$(for i in $d; do echo ${i:2:$DIR_NAME_LEN}; done)
-		# Get length of package name plus trailing '/'
-		PKG_NAME_LEN=${#PKG_NAME}
-		# Remove trailing '/'
-		PKG_NAME=$(for i in $PKG_NAME; do echo ${i:0:$PKG_NAME_LEN - 1}; done)
-		# Reset PKG_NAME_LEN to length of package name string
-		PKG_NAME_LEN=${#PKG_NAME}
+	# Get current directory string length
+	DIR_NAME_LEN=${#d}
+	# Remove './' from start of current directory
+	PKG_NAME=$(for i in $d; do echo ${i:2:$DIR_NAME_LEN}; done)
+	# Get length of package name plus trailing '/'
+	PKG_NAME_LEN=${#PKG_NAME}
+	# Remove trailing '/'
+	PKG_NAME=$(for i in $PKG_NAME; do echo ${i:0:$PKG_NAME_LEN - 1}; done)
+	# Reset PKG_NAME_LEN to length of package name string
+	PKG_NAME_LEN=${#PKG_NAME}
 
-		SEPARATOR_SIZE=`expr $PKG_NAME_LEN + 10`
-		MED_SEPARATOR=""
-		for i in $(seq 1 $SEPARATOR_SIZE); do
-			MED_SEPARATOR+="="
-		done
+	SEPARATOR_SIZE=`expr $PKG_NAME_LEN + 10`
+	MED_SEPARATOR=""
+	for i in $(seq 1 $SEPARATOR_SIZE); do
+		MED_SEPARATOR+="="
+	done
 
-		printf "${LIGHTCYAN}${SEPARATOR}${NC}\n\n"
-		printf "${LIGHTCYAN}${MED_SEPARATOR}${NC}\n"		
-		printf "${WHITE}//=>${NC} ${LIGHTRED}${PKG_NAME}${NC} ${WHITE}<=//${NC}\n"
-		printf "${LIGHTCYAN}${MED_SEPARATOR}${NC}\n"
-		printf "${YELLOW}Pulling from git repository...${NC}\n"
-		# Store results of git pull for formatted output
-		PULL=$(git pull)
-		if [ "$PULL" == "Already up to date." ]; then
-			printf "${LIGHTGREEN}${PULL}${NC}"
-		elif [ "$PULL" == *"fatal" ]; then
-			printf "${LIGHTRED}${PULL}${NC}"
-		else
-			printf "${LIGHTCYAN}${PULL}${NC}"
-		fi
-		echo && echo # New lines
+	printf "${LIGHTCYAN}${SEPARATOR}${NC}\n\n"
+	printf "${LIGHTCYAN}${MED_SEPARATOR}${NC}\n"		
+	printf "${WHITE}//=>${NC} ${LIGHTRED}${PKG_NAME}${NC} ${WHITE}<=//${NC}\n"
+	printf "${LIGHTCYAN}${MED_SEPARATOR}${NC}\n"
+	printf "${YELLOW}Pulling from git repository...${NC}\n"
+	# Store results of git pull for formatted output
+	PULL=$(git pull)
+	if [ "$PULL" == "Already up to date." ]; then
+		printf "${LIGHTGREEN}${PULL}${NC}"
+	elif [ "$PULL" == *"fatal" ]; then
+		printf "${LIGHTRED}${PULL}${NC}"
+	else
+		printf "${LIGHTCYAN}${PULL}${NC}"
+	fi
+	echo && echo # New lines
 
-		# Get pkgver from PKGBUILD, excluding all but the first match
-		NEW_VERSION=$(cat PKGBUILD | grep -m 1 "pkgver=")
-		# Strip "pkgver=" from version info
-		NEW_VERSION_LEN=${#NEW_VERSION}
-		NEW_VERSION=$(for i in $NEW_VERSION; do echo ${i:7:$NEW_VERSION_LEN}; done)
-		NEW_VERSION_LEN=${#NEW_VERSION}
+	# Get pkgver from PKGBUILD, excluding all but the first match
+	NEW_VERSION=$(cat PKGBUILD | grep -m 1 "pkgver=")
+	# Strip "pkgver=" from version info
+	NEW_VERSION_LEN=${#NEW_VERSION}
+	NEW_VERSION=$(for i in $NEW_VERSION; do echo ${i:7:$NEW_VERSION_LEN}; done)
+	NEW_VERSION_LEN=${#NEW_VERSION}
 
-		# Get currently installed package version from pacman
-		PACMAN_OUTPUT=$(pacman -Qe | grep $PKG_NAME)
-		# Strip package name from version info
-		CURRENT_VERSION=${PACMAN_OUTPUT:PKG_NAME_LEN}
+	# Get currently installed package version from pacman
+	PACMAN_OUTPUT=$(pacman -Qe | grep $PKG_NAME)
+	# Strip package name from version info
+	CURRENT_VERSION=${PACMAN_OUTPUT:PKG_NAME_LEN}
+	CURRENT_VERSION_LEN=`echo -n $CURRENT_VERSION | wc -m`
+	# Remove trailing "-1", etc. from currently installed version info
+	if [[ $CURRENT_VERSION =~ .*-.* && "$CURRENT_VERSION_LEN" -ne "$NEW_VERSION_LEN" ]]; then
+		DIFF=`expr $NEW_VERSION_LEN - $CURRENT_VERSION_LEN`
+		CURRENT_VERSION=${CURRENT_VERSION:0:DIFF}
 		CURRENT_VERSION_LEN=`echo -n $CURRENT_VERSION | wc -m`
-		# Remove trailing "-1", etc. from currently installed version info
-		if [[ $CURRENT_VERSION =~ .*-.* && "$CURRENT_VERSION_LEN" -ne "$NEW_VERSION_LEN" ]]; then
-			DIFF=`expr $NEW_VERSION_LEN - $CURRENT_VERSION_LEN`
-			CURRENT_VERSION=${CURRENT_VERSION:0:DIFF}
-			CURRENT_VERSION_LEN=`echo -n $CURRENT_VERSION | wc -m`
-		fi
+	fi
 
-		# If currently-installed version is the same as the new version,
-		# offer to rebuild; otherwise, offer to update
-		# Set verb variables accordingly
-		printf "${LIGHTCYAN}New Version Number:${NC}     ${LIGHTGREEN}${NEW_VERSION}${NC}\n"
-		if [[ "$CURRENT_VERSION" =~ "$NEW_VERSION" ]]; then
-			OPTION="REBUILD"
-			OPT_GERUND="Rebuilding"
-			printf "${LIGHTCYAN}Current Version Number:${NC}${LIGHTGREEN}${CURRENT_VERSION}${NC}\n"
-		else
-			OPTION="UPDATE"
-			OPT_GERUND="Updating"
-			printf "${LIGHTCYAN}Current Version Number:${NC} ${YELLOW}${CURRENT_VERSION}${NC}\n"
-		fi
-		echo # New line
+	# If currently-installed version is the same as the new version,
+	# offer to rebuild; otherwise, offer to update
+	# Set verb variables accordingly
+	printf "${LIGHTCYAN}New Version Number:${NC}     ${LIGHTGREEN}${NEW_VERSION}${NC}\n"
+	if [[ "$CURRENT_VERSION" =~ "$NEW_VERSION" ]]; then
+		OPTION="REBUILD"
+		OPT_GERUND="Rebuilding"
+		printf "${LIGHTCYAN}Current Version Number:${NC}${LIGHTGREEN}${CURRENT_VERSION}${NC}\n"
+	else
+		OPTION="UPDATE"
+		OPT_GERUND="Updating"
+		printf "${LIGHTCYAN}Current Version Number:${NC} ${YELLOW}${CURRENT_VERSION}${NC}\n"
+	fi
+	echo # New line
 
 
-		# Grab contents of PKGBUILD
-		# and print for review
-		printf "${LIGHTCYAN}${MINI_SEPARATOR}${NC}\n"
-		printf "${WHITE}//=> ${NC}${LIGHTCYAN}PKGBUILD${NC}${WHITE} <=//${NC}\n"
-		printf "${LIGHTCYAN}${MINI_SEPARATOR}${NC}\n"
-		PKG=$(cat PKGBUILD)
-		printf "${WHITE}${PKG}${NC}"
-		echo && echo # New lines
+	# Grab contents of PKGBUILD
+	# and print for review
+	printf "${LIGHTCYAN}${MINI_SEPARATOR}${NC}\n"
+	printf "${WHITE}//=> ${NC}${LIGHTCYAN}PKGBUILD${NC}${WHITE} <=//${NC}\n"
+	printf "${LIGHTCYAN}${MINI_SEPARATOR}${NC}\n"
+	PKG=$(cat PKGBUILD)
+	printf "${WHITE}${PKG}${NC}"
+	echo && echo # New lines
 
-		# Offer to rebuild or update package
-		# 'Y' or 'y' will accept, any other keypress will reject
-		read -p "$(printf "${LIGHTGREEN}//=> ${NC}${WHITE}Would you like to${NC} ${LIGHTCYAN}${OPTION}${NC} ${LIGHTRED}${PKG_NAME}${NC}${WHITE}?${NC} [Y/n]")" -r
-		if [[ $REPLY =~ ^[Yy]$ ]]; then
-			echo
-			printf "${LIGHTGREEN}${OPT_GERUND} ${PKG_NAME}!${NC}\n"
-			makepkg -sirc
-		else
-			# Convert OPT_GERUND to lower case
-			OPT_GERUND=$(echo $OPT_GERUND | tr '[:upper:]' '[:lower:]')
-			printf "${LIGHTRED}Not ${OPT_GERUND} ${PKG_NAME}!${NC}\n"
-			echo && echo
-		fi
+	# Offer to rebuild or update package
+	# 'Y' or 'y' will accept, any other keypress will reject
+	read -p "$(printf "${LIGHTGREEN}//=> ${NC}${WHITE}Would you like to${NC} ${LIGHTCYAN}${OPTION}${NC} ${LIGHTRED}${PKG_NAME}${NC}${WHITE}?${NC} [Y/n]")" -r
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		echo
+		printf "${LIGHTGREEN}${OPT_GERUND} ${PKG_NAME}!${NC}\n"
+		makepkg -sirc
+	else
+		# Convert OPT_GERUND to lower case
+		OPT_GERUND=$(echo $OPT_GERUND | tr '[:upper:]' '[:lower:]')
+		printf "${LIGHTRED}Not ${OPT_GERUND} ${PKG_NAME}!${NC}\n"
+		echo && echo
+	fi
 
-		# Go back to top level directory for next iteration
-		cd ..;
+	# Go back to top level directory for next iteration
+	cd ..;
 done
