1e495f61c88f2dcd9fc45331c208046b04eaa8db
William Dana <william.b.dana@gmail.com>
2018-09-14 00:45:22 -0400

    Work on installer script

--- install.sh
+++ install.sh
@@ -5,21 +5,32 @@
 
 install_aur_update ()
 {
-	# Add specified aur path to AUR-Update script
-	sed -i "2i aurpath=${aurpath}" ./aur-update
-	# Copy AUR-Update script to /usr/local/bin/
-	cp ./aur-update /usr/local/bin/aur-update
+	# echo "Add specified aur path to AUR-Update script..."
+	# sed -i "2i aurpath=${aurpath}" ./aur-update
+	# echo "Making ~/.aur-update if not exists..."
+	# mkdir -p $HOME/.aur-update
+	# echo "Copying AUR-Update script to ~/.aur-update"
+	# cp ./aur-update $HOME/.aur-update
+	# echo "Adding ~./aur-update to PATH"
+	# sed -i "2i export PATH=$HOME/.aur-update:\$PATH" $shell_configuration_file
+	# echo "Sourcing $shell_configuration_file for immediate use"
+	# source $shell_configuration_file
 }
 
 LIGHTRED='\033[1;31m'
 NC='\033[0m'
 # Prompt user for path to AUR package directory
-read -p "$(printf "${LIGHTRED}//=> Please enter the path to your AUR package directory${NC}\ne.g., ~/aur-packages ")" aurpath
+read -p "$(printf "${LIGHTRED}//=> Please enter the path to your AUR package directory${NC}\ne.g., ~/aur-packages \n")" aurpath
 
 read -p "$(printf "//=> You entered: \n ${LIGHTRED}${aurpath}${NC}\nIs this correct? [Y/n] ")" -r
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-	echo "Installing..."
-	install_aur_update
+	read -p "$(printf "${LIGHTRED}//=> Please enter the path to your shell configuration file${NC}\ne.g., ${LIGHTRED}~/.bash_profile${NC} or ${LIGHTRED} ~/.zshrc${NC} ")" shell_configuration_file
+
+	read -p "$(printf "//=> You entered: \n ${LIGHTRED}${shell_configuration_file}${NC}\nIs this correct? [Y/n] ")" -r
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		echo "Installing..."
+		install_aur_update
+	fi
 else
 	echo "Incorrect path to AUR package directory, aborting. Please try again!"
 fi
