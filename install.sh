0f0d77fdeb0d84e50fa229fb119b5ff823f4b188
William Dana <william.b.dana@gmail.com>
2019-03-07 16:25:37 -0500

    Update installer script to use ~/.aur-update and ~/.aur-update/cache

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 install_aur_update ()
 {
-	echo "Add specified aur path to AUR-Update script..."
+	echo "Adding specified aur path to AUR-Update script..."
 	sed -i "2i aurpath=${aurpath}" ./aur-update
 	echo "Making ~/.aur-update if not exists..."
 	mkdir -p $HOME/.aur-update
@@ -11,28 +11,26 @@ install_aur_update ()
 	echo "Adding ~./aur-update to PATH..."
 	sed -i "1i # AUR-Update to \$PATH" "$shell_configuration_file"
 	sed -i "2i export PATH=\$HOME/.aur-update:\$PATH" "$shell_configuration_file"
+	echo "Creating ~/.aur-update/cache if not exists..."
+	mkdir -p $cachepath
 	echo "Please restart your current shell or terminal session to enable aur-update"
 }
 
 LIGHTRED='\033[1;31m'
 NC='\033[0m'
 
-# Prompt user for path to AUR package directory
-read -p "$(printf "${LIGHTRED}//=> Please enter the EXACT path to your AUR package directory${NC}\ne.g., /home/wbdana/aur-packages \n")" aurpath
+aurpath=$HOME/.aur-update
+cachepath=$aurpath/cache
 
-read -p "$(printf "//=> You entered: \n ${LIGHTRED}${aurpath}${NC}\nIs this correct? [Y/n] ")" -r
+read -p "$(printf "${LIGHTRED}//=> Please enter the EXACT path to your preferred shell configuration file${NC}\ne.g., ${LIGHTRED}/home/wbdana/.bash_profile${NC} or ${LIGHTRED}/home/wbdana/.zshrc${NC} ")" shell_configuration_file
+
+read -p "$(printf "//=> You entered: \n ${LIGHTRED}${shell_configuration_file}${NC}\nIs this correct? [Y/n] ")" -r
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-	read -p "$(printf "${LIGHTRED}//=> Please enter the EXACT path to your shell configuration file${NC}\ne.g., ${LIGHTRED}/home/wbdana/.bash_profile${NC} or ${LIGHTRED}/home/wbdana/.zshrc${NC} ")" shell_configuration_file
-
-	read -p "$(printf "//=> You entered: \n ${LIGHTRED}${shell_configuration_file}${NC}\nIs this correct? [Y/n] ")" -r
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		echo "Installing..."
-		install_aur_update
-	fi
+	echo "Installing..."
+	install_aur_update
 else
-	echo "Incorrect path to AUR package directory, aborting. Please try again!"
+	echo "Incorrect path to shell configuration file specified; aborting. Please try again!"
 fi
-
 
 echo "AUR-Update installation complete."
 exit 0
